9da7c07fe75ef4a9bba64615b39d1c65bf6944c4
muslima <muslimahamu6@gmail.com>
2026-05-23 16:20:22 +0300

    up to date

--- tests/test_eda_utils.py
+++ tests/test_eda_utils.py
@@ -2,13 +2,11 @@ import pytest
 import pandas as pd
 from src.eda_utils import EDAAnalyzer
 from src.data_loader import create_sample_data
-
 def test_loss_ratio_calculation():
     df = create_sample_data()
     analyzer = EDAAnalyzer(df)
     loss_ratio = analyzer.calculate_loss_ratio()
     assert 0 <= loss_ratio <= 1
-
 def test_outlier_detection():
     df = create_sample_data()
     analyzer = EDAAnalyzer(df)
