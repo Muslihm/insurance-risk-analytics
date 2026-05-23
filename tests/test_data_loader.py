7aec1c874dbc8968e76fdfcc9085883518fb86e2
muslima <muslimahamu6@gmail.com>
2026-05-23 16:04:23 +0300

    test_data_loader.py updated

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -3,11 +3,11 @@ import pandas as pd
 from src.data_loader import InsuranceDataLoader, create_sample_data
 
 def test_create_sample_data():
-df = create_sample_data()
-assert len(df) == 1000
-assert 'TotalPremium' in df.columns
-assert 'TotalClaims' in df.columns
+    df = create_sample_data()
+    assert len(df) == 1000
+    assert 'TotalPremium' in df.columns
+    assert 'TotalClaims' in df.columns
 
 def test_insurance_data_loader():
-loader = InsuranceDataLoader()
-assert loader.data_path.name == 'insurance_claims.csv'
+    loader = InsuranceDataLoader()
+    assert loader.data_path.name == 'insurance_claims.csv'
